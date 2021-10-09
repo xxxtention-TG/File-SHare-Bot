72b94d579c509c13aa1452d9c3fa62e85780c5a6
XXXTENTACION <88360151+xxxtention-TG@users.noreply.github.com>
2021-10-09 17:39:06 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='https://t.me/darkz_angel'>Dᴀʀᴋ Aɴɢᴇʟ</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : 🔐\n○ Cʜᴀɴɴᴇʟ : <a href='https://t.me/darkz_angel'>Dᴀʀᴋ Aɴɢᴇʟ</a>\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : <a href='https://t.me/MOVIESHUB_GROUP'>Mᴏᴠɪᴇs Hᴜʙ</a></b>",
+            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='https://t.me/darkz_angel'>Dᴀʀᴋ Aɴɢᴇʟ</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : 🔐\n○ Cʜᴀɴɴᴇʟ : <a href='https://t.me/movies_hub66'>Mᴏᴠɪᴇs Hᴜʙ Cʜɴɴʀʟ</a>\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : <a href='https://t.me/MOVIESHUB_GROUP'>Mᴏᴠɪᴇs Hᴜʙ</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
